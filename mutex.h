bbd9efc7c5ba6d5bf9a090e63f2e3a8db9e35030
John Wonson <jmpwonson@gmail.com>
2020-12-21 17:03:24 +1100

    Create mutex.h

---
+++ mutex.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <mutex>
+
+template <typename T>
+class MutexGuard
+{
+    T& val;
+    std::mutex _mtx;
+    std::lock_guard<std::mutex> _guard;
+
+    public:
+        MutexGuard(T& val) : val{ val }, _guard{ _mtx } {}
+
+        MutexGuard& operator=(MutexGuard&&) = default;
+        MutexGuard& operator=(MutexGuard const&) = delete;
+
+        MutexGuard(MutexGuard&& other) : val{ other.val }, _guard{ _mtx } {}
+
+        MutexGuard(const MutexGuard&) = delete;
+
+        T& get() { return val; }
+        T& get() const { return val; }
+
+        T* operator->() { return &val; }
+        T* operator->() const { return &val; }
+
+        T& operator*() { return val; }
+        T& operator*() const { return val; }
+
+};
+
+template <typename T>
+class Mutex
+{
+    T _val;
+
+    public:
+        Mutex() : _val{} {}
+        Mutex(T const& val) : _val{ val } {}
+
+        MutexGuard<T> acquire() { return { _val }; }
+        MutexGuard<T> acquire() const { return { _val }; }
+};
